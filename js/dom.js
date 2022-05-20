8f385a9c49d8fd3ce118922193c344fc07834da5
Jonas Fernandez <hemm_@hotmail.com>
2022-05-20 11:16:05 -0300

    agregamos la explicacion de como agregar elementos y fragmentos desde el DOM al documento HTML

--- js/dom.js
+++ js/dom.js
@@ -158,3 +158,79 @@ API para documentos HTML y XML.
  console.log($cards.children[3].closest("section")) */
 
  //CREANDO ELEMENTOS Y FRAGMENTOS
+
+ const $figure =document.createElement("figure"),
+       $img= document.createElement("img"),
+       $figcaption= document.createElement("figcaption"),
+       $figcaptionText= document.createTextNode("Animals"),
+       $cards=document.querySelector(".cards"),
+       $figure2=document.createElement("figure")
+
+$img.setAttribute("src","https://placeimg.com/200/200/animals")
+$img.setAttribute("alt","Animals")
+$figure.classList.add("card")
+
+$figcaption.appendChild($figcaptionText)
+$figure.appendChild($img)
+$figure.appendChild($figcaption)
+$cards.appendChild($figure)
+
+$figure2.innerHTML= `
+<img src="https://placeimg.com/200/200/people" alt="People">
+<figcaption>People</figcaption>
+`
+
+$figure2.classList.add("card")
+$cards.appendChild($figure2)
+
+const estaciones = ["Primavera","Verano","Otoño","Invierno"],
+$ul = document.createElement("ul");
+document.write(`<h3>Estaciones del año</h3>`)
+document.body.appendChild($ul)
+
+estaciones.forEach((el)=>{
+    const $li =document.createElement("li");
+    $li.textContent = el ;
+    $ul.appendChild($li)
+})
+
+const continentes =["America","Africa","Asia","Europa","Oceania"],
+$ul2=document.createElement("ul")
+
+document.write(`<H3>Continentes</H3>`)
+document.body.appendChild($ul2)
+$ul2.innerHTML="";
+continentes.forEach((el)=>{
+    $ul2.innerHTML+=`<li>${el}</li>`});
+
+document.write(`<h3>Estaciones del año</h3>`)
+
+const meses= [
+    "enero",
+    "febrero",
+    "marzo",
+    "abril",
+    "mayo",
+    "junio",
+    "julio",
+    "agosto",
+    "setiembre",
+    "octubre",
+    "noviembre",
+    "diciembre"
+    
+];
+const $ul3= document.createElement("ul"),
+$fragment = document.createDocumentFragment();
+
+meses.forEach((el)=>{
+     const $li=document.createElement("li")
+     $li.textContent=el
+     $fragment.appendChild($li)
+})
+
+$ul3.appendChild($fragment)
+document.body.appendChild($ul3)
+
+
+
