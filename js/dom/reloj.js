2c5bcc0857ef03d1df023afc11b4f659f6775847
Jonas Fernandez <hemm_@hotmail.com>
2022-05-23 13:27:43 -0300

    reloj terminado[mio]

---
+++ js/dom/reloj.js
@@ -0,0 +1,62 @@
+export default function clockAlarm(clock,alarmSound,timeInit,timeStop,alarmStart,alarmStop){
+    
+ const d=document
+
+ function reloj (){
+
+  let fecha=new Date()
+  d.querySelector(clock).innerHTML= fecha;
+  }
+        
+ window.onload=function(){
+
+  setInterval(() => {
+   reloj() 
+  }, 1000);
+  }
+    
+
+ d.addEventListener("click",(e)=>{
+        
+  if(e.target.matches(timeInit)){
+        
+   d.querySelector(clock).classList.remove("display-none");
+       
+   d.querySelector(timeInit).disabled=true; 
+
+ }
+ 
+
+  if(e.target.matches(timeStop)){
+
+   d.querySelector(clock).classList.add("display-none");
+
+   d.querySelector(timeInit).disabled=false 
+ }
+
+ if(e.target.matches(alarmStart)){
+   
+
+   d.querySelector(alarmSound).play()
+   
+   d.querySelector(alarmStart).disabled=true;
+
+
+ }
+
+  if(e.target.matches(alarmStop)){
+
+    d.querySelector(alarmSound).pause()
+    
+    d.querySelector(alarmSound).currentTime=0;
+   
+    d.querySelector(alarmStart).disabled=false
+    
+  }
+  
+ 
+  
+
+ })
+}
+
--- js/module.js
+++ js/module.js
@@ -1,9 +1,17 @@
 import hamburguesaMenu from "./dom/menuhamburgesa.js";
+import clockAlarm from "./dom/reloj.js";
 
 const d=document
 
 d.addEventListener("DOMContentLoaded",(e)=>{
-    hamburguesaMenu(".hamburger",".menu-container",".menu-container a")
-
+    hamburguesaMenu(".hamburger",".menu-container",".menu-container a");
+    clockAlarm(".clock",".alarm-sound",".timestart",".timestop",".alarmstart",".alarmstop")    
 });
 
+
+
+
+
+
+
+ 
