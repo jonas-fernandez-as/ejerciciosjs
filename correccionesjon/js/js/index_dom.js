169cd865d45faea33566186dec23893baaa2ede4
Jonas Fernandez <hemm_@hotmail.com>
2022-05-24 13:37:39 -0300

    agregamos la correccion a los eventos del teclado de jon

--- correccionesjon/js/js/index_dom.js
+++ correccionesjon/js/js/index_dom.js
@@ -1,9 +1,19 @@
 import hamburgerMenu from "./dom/menu_hamburguesa.js";
 import { digitalClock,alarm } from "./dom/reloj.js";
+import { moveBall, shortcuts } from "./dom/teclado.js";
 const d = document;
 
 d.addEventListener("DOMContentLoaded",e=>{
 hamburgerMenu(".panel-btn",".panel",".menu a");
 digitalClock("#reloj","#activar-reloj","#desactivar-reloj")
 alarm("assets/alarma-morning-mix.mp3","#activar-alarma","#desactivar-alarma")
+
 });
+
+
+
+d.addEventListener("keydown",(e) =>{
+    shortcuts(e);
+    moveBall(e,".ball",".stage");
+}
+);
