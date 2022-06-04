2e8e3da4ab24b5da4ce004fbd3a9ed7779697514
Jonas Fernandez <hemm_@hotmail.com>
2022-06-04 11:42:48 -0300

    agregando la correccion de scroll spy de jon

--- correccionesjon/js/js/index_dom.js
+++ correccionesjon/js/js/index_dom.js
@@ -14,6 +14,7 @@ import getGeolocation from "./dom/geolocalizacion.js";
 import searchFiles from "./dom/filtro_busquedas.js";
 import draw from "./dom/sorteo.js";
 import slider from "./dom/carrusel.js";
+import scrollSpy from "./dom/scroll_espia.js";
 const d = document;
 
 d.addEventListener("DOMContentLoaded",e=>{
@@ -41,6 +42,7 @@ getGeolocation("geolocation")
 searchFiles(".card-filter",".card")
 draw("#winner-btn",".player")
 slider()
+scrollSpy()
 
 });
 
