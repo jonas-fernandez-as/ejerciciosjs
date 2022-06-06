3368ce67a03ea7707796cb73d2b2d0698b97a506
Jonas Fernandez <hemm_@hotmail.com>
2022-06-06 17:28:52 -0300

    TERMINAMOS CON LA VALIDACION DE FORMULARIOS DE JAVASCRIPT

--- correccionesjon/js/js/index_dom.js
+++ correccionesjon/js/js/index_dom.js
@@ -16,6 +16,7 @@ import draw from "./dom/sorteo.js";
 import slider from "./dom/carrusel.js";
 import scrollSpy from "./dom/scroll_espia.js";
 import smartVideo from "./dom/video_inteligente.js";
+import contactFormValidations from "./dom/validaciones_formularios.js";
 const d = document;
 
 d.addEventListener("DOMContentLoaded",e=>{
@@ -45,6 +46,7 @@ draw("#winner-btn",".player")
 slider()
 scrollSpy()
 smartVideo()
+contactFormValidations()
 
 });
 
